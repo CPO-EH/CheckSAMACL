41ca2bf9b3a2d01518976b9757f7d7927a81d17b
CPO-EH <56457033+CPO-EH@users.noreply.github.com>
2021-07-20 20:20:32 +0200

    Create CheckSAMACL.ps1

---
+++ CheckSAMACL.ps1
@@ -0,0 +1,5 @@
+﻿$Detect = @("builtin\users","builtin\utilisateurs")
+$CheckACL = (Get-Acl "$env:SystemDrive\windows\system32\config\sam").Access
+$Detected = $false
+foreach ($IDRef in $CheckACL){if ($IDRef.IdentityReference.Value.ToLower() -in $Detect -and $IDRef.FileSystemRights.ToString().ToLower().Contains("read")){$Detected = $true}}
+return $Detected
