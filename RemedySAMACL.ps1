58b1dadb6ff73cb1e46518ab0e953e0e8ee46af8
CPO-EH <56457033+CPO-EH@users.noreply.github.com>
2021-07-21 10:30:28 +0200

    Update RemedySAMACL.ps1

--- RemedySAMACL.ps1
+++ RemedySAMACL.ps1
@@ -1,12 +1,3 @@
 #Remedy
-$Detect = @("builtin\users","builtin\utilisateurs")
-$CheckACL = Get-Acl "$env:SystemDrive\windows\system32\config\sam"
-$Files = @("sam","security","system")
-foreach ($access in $CheckACL.Access){
-    if ($access.IdentityReference.Value -in $Detect){
-        foreach ($File in $Files){
-            & icacls "$env:SystemDrive\windows\system32\config\$($File)" /inheritance:d # Had to Break Inheritance
-            & icacls "$env:SystemDrive\windows\system32\config\$($File)" /remove $access.IdentityReference.Value
-        }
-    }
-}
+& icacls $env:SystemDrive\windows\system32\config\*.* /inheritance:e
+& vssadmin delete shadows /for=$env:SystemDrive /Quiet
